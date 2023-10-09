60c101296a388e9fad601ce9099f7faef9ec625d
Navneet <navneet.patwal31@gmail.com>
2023-10-09 23:51:20 +0530

    Order It done

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+      <footer className='text-center'>
+        <p className='text-center mt-1'>
+            Food Delivery Website - 2023-2024, All rights Reserved..
+        </p>
+      </footer>
+    </>
+  )
+}
